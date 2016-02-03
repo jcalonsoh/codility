a1594af6b769774ae7c2ed7ceb2bdec1dfb19abd
Juan Carlos Alonso Holmstron <j.carlos.alonso.h@gmail.com>
2016-02-03 00:01:34 -0300

    Next sibling

--- sibling.rb
+++ sibling.rb
@@ -1,4 +1,4 @@
-def next_sibling(a)
+def largest_sibling(a)
   for b in (a..10_000) do
     c = b if b.real?
     sibling = c if a.to_s.chars.map(&:to_i).sort == c.to_s.chars.map(&:to_i).sort
@@ -10,9 +10,9 @@ require 'minitest/autorun'
 
 class Tests < MiniTest::Unit::TestCase
   def test_example_input
-    assert_equal 321, next_sibling(123)
+    assert_equal 321, largest_sibling(123)
   end
   def test_example_input_2
-    assert_equal 553, next_sibling(553)
+    assert_equal 553, largest_sibling(553)
   end
 end
